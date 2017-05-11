6fa244a799a752e14f1a38bfdf95db09ef5b5ff6
alexador <karmiceft@gmail.com>
2017-05-12 02:13:54 +0300

    Add files via upload

---
+++ static/js/separate-js/my.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  // Wow Animations
+  let wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true // default
+  });
+  wow.init();
+
+
+  // Magnific Popup
+  $('.popup-youtube').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
+});
